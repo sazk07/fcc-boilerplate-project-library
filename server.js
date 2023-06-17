435ac737e21fe9436cf76f68991cbee5c5d57654
shahan <shahan@gmail.com>
2023-06-18 04:55:32 +0500

    feat: mongoose connection

--- server.js
+++ server.js
@@ -1,56 +1,62 @@
 'use strict';
 
 const express     = require('express');
-const bodyParser  = require('body-parser');
 const cors        = require('cors');
 require('dotenv').config();
 
 const apiRoutes         = require('./routes/api.js');
 const fccTestingRoutes  = require('./routes/fcctesting.js');
 const runner            = require('./test-runner');
+const console = require('zombie/lib/console.js');
+const { default: mongoose } = require('mongoose');
 
 const app = express();
 
-app.use('/public', express.static(process.cwd() + '/public'));
+main().catch(err => console.error(err))
+async function main() {
+  await mongoose.connect(process.env.DB)
 
-app.use(cors({origin: '*'})); //USED FOR FCC TESTING PURPOSES ONLY!
+  app.use('/public', express.static(process.cwd() + '/public'));
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(cors({origin: '*'})); //USED FOR FCC TESTING PURPOSES ONLY!
 
-//Index page (static HTML)
-app.route('/')
-  .get(function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
+    app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+
+  //Index page (static HTML)
+  app.route('/')
+    .get(function (req, res) {
+      res.sendFile(process.cwd() + '/views/index.html');
+    });
+
+  //For FCC testing purposes
+  fccTestingRoutes(app);
+
+  //Routing for API
+  apiRoutes(app);
+
+  //404 Not Found Middleware
+  app.use(function(req, res, next) {
+    res.status(404)
+      .type('text')
+      .send('Not Found');
   });
 
-//For FCC testing purposes
-fccTestingRoutes(app);
-
-//Routing for API 
-apiRoutes(app);  
-    
-//404 Not Found Middleware
-app.use(function(req, res, next) {
-  res.status(404)
-    .type('text')
-    .send('Not Found');
-});
-
-//Start our server and tests!
-const listener = app.listen(process.env.PORT || 3000, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
-  if(process.env.NODE_ENV==='test') {
-    console.log('Running Tests...');
-    setTimeout(function () {
-      try {
-        runner.run();
-      } catch(e) {
-          console.log('Tests are not valid:');
-          console.error(e);
+  //Start our server and tests!
+    const listener = app.listen(process.env.PORT || 3000, function () {
+      console.log('Your app is listening on port ' + listener.address().port);
+      if(process.env.NODE_ENV==='test') {
+        console.log('Running Tests...');
+        setTimeout(function () {
+          try {
+            runner.run();
+          } catch(e) {
+            console.log('Tests are not valid:');
+            console.error(e);
+          }
+        }, 1500);
       }
-    }, 1500);
-  }
-});
+    });
+}
 
 module.exports = app; //for unit/functional testing
